760594de83537f7771aa67adb1235941340647d3
LEGRAND Matthieu <legrand.ma@chu-toulouse.fr>
2023-03-15 12:44:44 +0100

    accept bug fix

--- yew-html-attributes-macro-derive/src/utils.rs
+++ yew-html-attributes-macro-derive/src/utils.rs
@@ -8,7 +8,9 @@ lazy_static! {
 
     // Load the fields from the csv html-attributes-general.csv
     let bytes = include_bytes!("html-attributes.csv");
-    let mut rdr = csv::Reader::from_reader(bytes.as_slice());
+    let mut rdr = csv::ReaderBuilder::new()
+      .has_headers(false)
+      .from_reader(bytes.as_ref());
 
     for line in rdr.records() {
       let line = line.expect("yew-attributes panicked : Internal error. Please raise an issue on the Github [code : trlzv]");
@@ -16,9 +18,9 @@ lazy_static! {
       let name = line.get(0).expect("yew-attributes panicked : Internal error. Please raise an issue on the Github [code : kcwlc]");
       let usages = line.get(1).expect("yew-attributes panicked : Internal error. Please raise an issue on the Github [code : kcwld]");
       let typ = line.get(2).expect("yew-attributes panicked : Internal error. Please raise an issue on the Github [code : yynya]");
-
+      
       let usages = usages.split(','); 
-
+      
       for usage in usages {
         let usage = usage.to_string();
         html_attributes.entry(usage).or_insert(HashMap::new()).insert(name.to_string(), typ.to_string());
